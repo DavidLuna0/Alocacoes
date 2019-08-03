787240f72ae295d51695cd879b359b74b36c89b0
DavidLuna0 <ddavidr569@gmail.com>
2019-08-03 19:35:30 -0300

    Criando get, post, put e delete de skills

---
+++ app/controllers/skill.js
@@ -0,0 +1,47 @@
+const { Skill } = require('../models');
+
+module.exports.getAllSkills = async (application, req, res) => {
+    Skill.findAll().then(result => {
+        res.status(200).send(result);
+    }).catch(err => {
+        res.status(400).send({error: err});
+    })
+}
+
+module.exports.getSkillById = async (application, req, res) => {
+    await Skill.findByPk(req.params.id).then(result => {
+        res.status(200).send(result);
+    }).catch(err => {
+        res.status(400).send({error: err});
+    })
+}
+
+module.exports.postSkill = async (application, req, res) => {
+    const nome = req.body;
+    await Skill.create(nome).then(result => {
+        res.status(200).send(result);
+    }).catch(err => {
+        res.status(400).send({error: err});
+    });
+}
+
+module.exports.putSkill = async (application, req, res) => {
+    const nome = req.body;
+    const id = req.params.id;
+    await Skill.update(nome, {where: {id}}).then(result => {
+        res.status(201).send(result);
+    }).catch(err => {
+        res.send({error: err});
+    })
+}
+
+module.exports.deleteSkill = async (application, req, res) => {
+    const id = req.params.id;
+    Skill.findByPk(id).then(skill => {
+        skill.destroy().then(result => {
+            res.send(result);
+        });
+    }).catch(err => {
+        res.send({error: err})
+    })
+}
